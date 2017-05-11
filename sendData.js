162da8ee8057b08fbae5ce2ea1ba2a23a0c266b8
zh970205@outlook.com <zh19970205@126.com>
2017-05-12 05:30:53 +0800

    html

--- sendData.js
+++ sendData.js
@@ -1,72 +1,75 @@
-var sender = (x,y,color)=>fetch(
-  'http://canvas.ourscgy.ustc.edu.cn/canvas/modify',
-  {
-     headers:
-     {
-       'Accept': 'application/json',
-       'Content-Type': 'application/json'
-     },
-     method: "POST",
-     body: JSON.stringify({canvas:[{x,y,color}],count:50000})
-  }
-).then(
-  (res)=>res.json()
-).then(
-  json=>console.log(json.flag)
-)
-
-var crt = {}
-var exp = []
-var dif = []
-
-Promise.all([
-  fetch(
-    'http://canvas.ourscgy.ustc.edu.cn/canvas/update?count=-1'
+var runit(aim_url)=()=>{
+  var sender = (x,y,color)=>fetch(
+    'http://canvas.ourscgy.ustc.edu.cn/canvas/modify',
+    {
+       headers:
+       {
+         'Accept': 'application/json',
+         'Content-Type': 'application/json'
+       },
+       method: "POST",
+       body: JSON.stringify({canvas:[{x,y,color}],count:50000})
+    }
   ).then(
     (res)=>res.json()
   ).then(
-    json=>{
-      for(i of json.data){
-        crt[i.x+","+i.y]=i.color
-      }
-    }
-  ),
-  fetch(
-    'https://raw.githubusercontent.com/hzhangxyz/canvas/master/data'
-  ).then(
-    (res)=>res.text()
-  ).then(
-    text=>{
-      for(i of text.split("\n")){
-        var p = i.split("\t")
-        if(p.length!=3){
-          continue
+    json=>console.log(json.flag)
+  )
+
+  var crt = {}
+  var exp = []
+  var dif = []
+
+  Promise.all([
+    fetch(
+      'http://canvas.ourscgy.ustc.edu.cn/canvas/update?count=-1'
+    ).then(
+      (res)=>res.json()
+    ).then(
+      json=>{
+        for(i of json.data){
+          crt[i.x+","+i.y]=i.color
         }
-        exp.push(p)
+      }
+    ),
+    fetch(
+      aim_url
+    ).then(
+      (res)=>res.text()
+    ).then(
+      text=>{
+        for(i of text.split("\n")){
+          var p = i.split("\t")
+          if(p.length!=3){
+            continue
+          }
+          exp.push(p)
+        }
+      }
+    )
+  ]).then(
+    ()=>{
+      for(j of exp){
+        i = [parseInt(j[0]),parseInt(j[1]),parseInt(j[2])]
+        if(crt[i[0]+","+i[1]]!=i[2]){
+          dif.push(i)
+        }
+      }
+      console.log( "Num: "+(exp.length-dif.length)+" "+exp.length+"Rank: "+100.*(exp.length-dif.length)/exp.length+"%" )
+    }
+  ).then(
+    ()=>{
+      l = dif.length
+      for(var i=0;i<10;i++){
+        var idx = parseInt(Math.random()*l)
+        sender(
+          parseInt(dif[idx][0]),
+          parseInt(dif[idx][1]),
+          parseInt(dif[idx][2])
+        )
       }
     }
   )
-]).then(
-  ()=>{
-    for(j of exp){
-      i = [parseInt(j[0]),parseInt(j[1]),parseInt(j[2])]
-      if(crt[i[0]+","+i[1]]!=i[2]){
-        dif.push(i)
-      }
-    }
-    console.log( "Num: "+(exp.length-dif.length)+" "+exp.length+"Rank: "+100.*(exp.length-dif.length)/exp.length+"%" )
-  }
-).then(
-  ()=>{
-    l = dif.length
-    for(var i=0;i<10;i++){
-      var idx = parseInt(Math.random()*l)
-      sender(
-        parseInt(dif[idx][0]),
-        parseInt(dif[idx][1]),
-        parseInt(dif[idx][2])
-      )
-    }
-  }
-)
-
+}
+always=aim_url=>setInterval(runit(aim_url),120*1000)
+always('https://raw.githubusercontent.com/hzhangxyz/canvas/master/data')
