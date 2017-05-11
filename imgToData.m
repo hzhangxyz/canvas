70b37d3fd9947e1d75ebf8543f31df0002bb4c97
SpinningAleph0 <zh970205@outlook.com>
2017-05-12 05:47:17 +0800

    Update imgToData.m

--- imgToData.m
+++ imgToData.m
@@ -1,6 +1,6 @@
 data = ImageData[ImageResize[src, {80, 80}]];
 ans = Flatten[
-   Table[sample = data[[i, j]]; {i, j, 
-     Floor[((sample[[1]]*256 + sample[[2]])*256 + 
-         sample[[3]])*256]}, {i, 1, 80}, {j, 1, 80}], 1];
+   Table[sample = data[[i, j]]; {j + 120, i, 
+     Floor[(Floor[sample[[1]]*255]*256 + Floor[sample[[2]]*255])*256 +
+        Floor[sample[[3]]*255]]}, {i, 1, 40}, {j, 1, 30}], 1];
 Export["C:\\Users\\zh199\\Desktop\\test\\canvas\\data", ans, "Table"]
