a1530d3607053baa87e444e66df229b53431e6f7
trumpet <zh19970205@126.com>
2017-05-12 16:52:45 +0000

    proxy

--- sendData.py
+++ sendData.py
@@ -1,14 +1,17 @@
 
 crt = """curl 'http://canvas.ourscgy.ustc.edu.cn/canvas/update?count=-1' -H 'Pragma: no-cache' -H 'DNT: 1' -H 'Accept-Encoding: gzip, deflate, sdch' -H 'Accept-Language: zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36' -H 'Accept: application/json, text/plain, */*' -H 'Cache-Control: no-cache' -H 'X-Requested-With: XMLHttpRequest' -H 'Cookie: _ga=GA1.3.398996058.1493125830;iPlanetDirectoryPro=ucas-ccfa97864ba5f61ab069223a79d63271' -H 'Connection: keep-alive' -H 'Referer: http://canvas.ourscgy.ustc.edu.cn/' --compressed 2>/dev/null"""
-src = """curl 'http://canvas.ourscgy.ustc.edu.cn/canvas/modify' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36' -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' --data-binary '{"canvas":[{"x":%d,"y":%d,"color":%d}],"count":50000}' --compressed 2>/dev/null"""
+src = """curl -x %s -L 'http://canvas.ourscgy.ustc.edu.cn/canvas/modify' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36' -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' --data-binary '{"canvas":[{"x":%d,"y":%d,"color":%d}],"count":50000}' --compressed 2>/dev/null"""
 
 from subprocess import check_output as co
 from json import loads as parse
 from time import sleep
 from random import random
+import sys
+
+s = sys.argv[1]
 
 def send(x,y,color):
-    run = src%(x,y,color)
+    run = src%(s,x,y,color)
     # print "Run: ",run
     ans = co(run,shell=True)
     # print "Ans: ",ans
@@ -16,7 +19,7 @@ def send(x,y,color):
 
 data=parse(co(crt,shell=True))["data"]
 
-canvas = [[0 for j in xrange(200)] for i in xrange(200)]
+canvas = [[0 for j in xrange(300)] for i in xrange(300)]
 
 for i in data:
     canvas[i["x"]][i["y"]]=i["color"]
@@ -47,6 +50,7 @@ for i in open("data","r"):
         paint.append(data)
 
 l = len(paint)
+print l,ll
 print "Num: ",ll-l,ll,"Rank: ",100.*(ll-l)/ll,"%"
 
 if l == 0:
@@ -54,6 +58,4 @@ if l == 0:
 
 for i in xrange(10):
     idx = int(random()*l)
-    while (paint[idx][2]==16181480) and (canvas[paint[idx][0]][paint[idx][1]]==16382716) and (random()>0.2):
-        idx = int(random()*l)
     send(*paint[idx])
